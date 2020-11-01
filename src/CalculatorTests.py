d0bd1126affb8af4b1e3e9d5d77a0032934b33b3
dhavaldp96 <dp95@njit.edu>
2020-11-02 03:54:03 +0530

    Feature: Added Division Method to CalculatorTests

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -11,13 +11,11 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-
     def test_add_method_calculator(self):
         test_data = CsvReader("src/Unit Test Addition.csv").data
         for row in test_data:
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
-
 
     def test_sub_method_calculator(self):
         test_data = CsvReader("src/Unit Test Subtraction.csv").data
