33adc55e1acf50d081067c2b4f45430bf6d05ffe
dhavaldp96 <dp95@njit.edu>
2020-11-02 08:37:17 +0530

    Task: Added files to SRC Directory

---
+++ src/Calculator__Test.py
@@ -0,0 +1,21 @@
+import math
+
+
+def addition(a, b):
+    a = int(a)
+    b = int(b)
+    c = a + b
+    return c
+
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def addit(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+
