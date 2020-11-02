346dc342d5730a4de87500351b2f5668504cb79e
dhavaldp96 <dp95@njit.edu>
2020-11-02 08:48:25 +0530

    Task: Added Subtraction Method to the Calculator

--- src/Calculator_.py
+++ src/Calculator_.py
@@ -7,15 +7,29 @@ def addition(a, b):
     c = a + b
     return c
 
+def subtraction(a, b):
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
+
 
 class Calculator:
     result = 0
 
-    def __init__(self):
-        pass
+def __init__(self):
+    pass
 
-    def addit(self, a, b):
-        self.result = addition(a, b)
-        return self.result
+def addit(self, a, b):
+    self.result = addition(a, b)
+    return self.result
+
+
+def subtraction(a, b):
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
 
 
